1c461df19ea0c827e073083a9362815b8982f0b9
Bilalraza617 <anwarbilal617@gmail.com>
2024-05-30 10:56:24 +0500

    complete

--- day-06/q16/main.js
+++ day-06/q16/main.js
@@ -1,4 +1,18 @@
-var list = ["Bilal", "Sheriyans", "Chahat fateh ali khan"];
+// Q14 ans:
+var list = ["Hitesh chudray", "Sheriyans", "Chahat fateh ali khan"];
+list.forEach(function (name) {
+    console.log("Mr " +
+        name +
+        " meri taraf sy dinner ki dawat ko qabol kary or aj rat ka khan mery sath khaiy.");
+});
+//Q15 ans
+console.log("Suno logo ".concat(list[0], " dinner pr nahi aiy ga."));
+list[list.indexOf("Hitesh chudray")] = "Bilal";
+for (var _i = 0, list_1 = list; _i < list_1.length; _i++) {
+    var item = list_1[_i];
+    console.log("".concat(item, " bhai agr hitesh nahi araha to to kia howa ap ajao."));
+}
+//Q 16 ANS
 console.log("Suno suno I found a bigger dinner table.");
 // Add guest in biginnig
 list.unshift("Hitesh chudary");
--- day-06/q16/main.ts
+++ day-06/q16/main.ts
@@ -1,4 +1,26 @@
-const list: string[] = ["Bilal", "Sheriyans", "Chahat fateh ali khan"];
+// Q14 ans:
+
+let list: string[] = ["Hitesh chudray", "Sheriyans", "Chahat fateh ali khan"];
+
+list.forEach((name) => {
+  console.log(
+    "Mr " +
+      name +
+      " meri taraf sy dinner ki dawat ko qabol kary or aj rat ka khan mery sath khaiy."
+  );
+});
+
+//Q15 ans
+
+console.log(`Suno logo ${list[0]} dinner pr nahi aiy ga.`);
+
+list[list.indexOf("Hitesh chudray")] = "Bilal";
+
+for (const item of list) {
+  console.log(`${item} bhai agr hitesh nahi araha to to kia howa ap ajao.`);
+}
+
+//Q 16 ANS
 
 console.log("Suno suno I found a bigger dinner table.");
 
