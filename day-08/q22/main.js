9b9e5c3b986d5edf4cc3f7668ce5568f9929f3e4
Bilalraza617 <anwarbilal617@gmail.com>
2024-06-02 03:33:14 +0500

    complete

---
+++ day-08/q22/main.js
@@ -0,0 +1,5 @@
+var list = ["Bilal", "Shahid", "Khalid"];
+//Give error
+console.log("Name :", list[3]);
+// Correct
+console.log("Name :", list[2]);
---
+++ day-08/q22/main.ts
@@ -0,0 +1,9 @@
+const list: string[] = ["Bilal", "Shahid", "Khalid"];
+
+//Give error
+
+console.log("Name :", list[3]);
+
+// Correct
+
+console.log("Name :", list[2]);
