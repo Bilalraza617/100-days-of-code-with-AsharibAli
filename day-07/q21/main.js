9e1535874e439f77621b78c81d87c0d2cb6a76ae
Bilalraza617 <anwarbilal617@gmail.com>
2024-06-01 07:03:46 +0500

    complete

---
+++ day-07/q21/main.js
@@ -0,0 +1,6 @@
+var firend = {
+    fr1: "BIlal",
+    fr2: "Raza",
+    fr3: "Owais",
+};
+console.log("My frinds : ", firend);
---
+++ day-07/q21/main.ts
@@ -0,0 +1,11 @@
+const firend: {
+  fr1: String;
+  fr2: String;
+  fr3: String;
+} = {
+  fr1: "BIlal",
+  fr2: "Raza",
+  fr3: "Owais",
+};
+
+console.log("My frinds : ", firend);
