1aa0bc138fd78d8e4f4062c1f65647e9e802ba74
Bilalraza617 <anwarbilal617@gmail.com>
2024-06-02 05:09:58 +0500

    complet

--- day-08/q23/main.js
+++ day-08/q23/main.js
@@ -1,39 +1,25 @@
-let fName: string = "Khurram";
-
+var fName = "Khurram";
 console.log("Is fName == 'Khurram' ? I predict true.");
 console.log(fName == "Khurram");
-
 console.log("Is fName == 'Bilal'? I predict false");
 console.log(fName == "Bilal");
-
-let lName: string = "Raza";
-
+var lName = "Raza";
 console.log("Is lName == 'Raza'? I predict true.");
 console.log(lName == "Raza");
-
 console.log("Is lName == 'khurshid'? I predict false.");
 console.log(lName == "khurshid");
-
-let lang: string = "English";
-
+var lang = "English";
 console.log("Is lang == 'English' ? I predict true.");
 console.log(lang == "English");
-
 console.log("Is lang == 'Urdu'? I predict false.");
 console.log(fName == "Urdu");
-
-let book: string = "Fatawa";
-
+var book = "Fatawa";
 console.log("Is book == 'fatawa'? I predict true.");
 console.log(book == "Fatawa");
-
 console.log("Is book == 'Nahaw'? I predict false.");
 console.log(book == "Nahaw");
-
-let teacher: string = "Sir kashif";
-
+var teacher = "Sir kashif";
 console.log("Is teacher == 'Sir kashif'? I predict true.");
 console.log(teacher == "Sir kashif");
-
 console.log("Is teacher == 'Sir sikandar'? I predict false.");
 console.log(teacher == "Sir sikandar");
