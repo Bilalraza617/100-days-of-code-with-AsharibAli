ad61ead4a7dfaea3fbc309b432188a1dbabc7c42
Bilalraza617 <anwarbilal617@gmail.com>
2024-06-27 13:47:21 +0500

    complete

---
+++ day-10/q29/main.js
@@ -0,0 +1,16 @@
+var favorite_fruits = ["Mango", "Water malon", "Banana"];
+if (favorite_fruits.includes("Mango")) {
+    console.log("You really like Mango.");
+}
+if (favorite_fruits.includes("Water malon")) {
+    console.log("You really like Water malon.");
+}
+if (favorite_fruits.includes("Banana")) {
+    console.log("You really like Banana.");
+}
+if (favorite_fruits.includes("Cherry")) {
+    console.log("You really like Cherry.");
+}
+if (favorite_fruits.includes("Apple")) {
+    console.log("You really like Apple.");
+}
