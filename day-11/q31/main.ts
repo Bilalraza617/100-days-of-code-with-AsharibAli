4bd03c1ff34918735e060498ab24c36ad4070077
Bilalraza617 <anwarbilal617@gmail.com>
2024-07-10 12:42:04 +0500

    complete

---
+++ day-11/q31/main.ts
@@ -0,0 +1,20 @@
+let user_name = [
+  "Bilal",
+  "Hussain",
+  "Qari",
+  "Samandar Khan",
+  "Admin",
+  "Bahadur Khan",
+  "Haji",
+  "Qasam",
+];
+
+user_name.forEach((user) => {
+  if (user === "Admin") {
+    console.log("Hello " + user + ", would you like to see a status report?");
+  } else console.log("Hello " + user + ",  thank you for logging in again.");
+});
+
+user_name.length = 0;
+
+if (user_name.length === 0) console.log("\nWe need to find some users!");
