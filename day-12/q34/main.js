ff8b485cb4a8f7c915ecea9d50e27e17b7f7e934
Bilalraza617 <anwarbilal617@gmail.com>
2024-07-13 07:39:09 +0500

    complete

---
+++ day-12/q34/main.js
@@ -0,0 +1,17 @@
+var pizzas = [
+    "Chicago Deep-Dish Pizza",
+    "Margherita Pizza",
+    "Sicilian Pizza",
+    "Hawaiian Pizza",
+    "California Pizza",
+    "Greek Pizza",
+];
+for (var _i = 0, pizzas_1 = pizzas; _i < pizzas_1.length; _i++) {
+    var pizza = pizzas_1[_i];
+    console.log(pizza);
+}
+for (var _a = 0, pizzas_2 = pizzas; _a < pizzas_2.length; _a++) {
+    var pizza = pizzas_2[_a];
+    console.log("I like ".concat(pizza, " pizza."));
+}
+console.log("I really love vagetable!");
---
+++ day-12/q34/main.ts
@@ -0,0 +1,18 @@
+let pizzas = [
+  "Chicago Deep-Dish Pizza",
+  "Margherita Pizza",
+  "Sicilian Pizza",
+  "Hawaiian Pizza",
+  "California Pizza",
+  "Greek Pizza",
+];
+
+for (let pizza of pizzas) {
+  console.log(pizza);
+}
+
+for (let pizza of pizzas) {
+  console.log(`I like ${pizza} pizza.`);
+}
+
+console.log("I really love vagetable!");
