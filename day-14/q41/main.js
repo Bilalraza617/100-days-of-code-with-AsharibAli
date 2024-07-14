76b3563e449a4c5f68d84050fa13498c0b6a8091
Bilalraza617 <anwarbilal617@gmail.com>
2024-07-14 07:52:15 +0500

    completet

---
+++ day-14/q41/main.js
@@ -0,0 +1,9 @@
+var magicion = [
+    "Chahat fateh ali khan",
+    "RAhat fateh ali khan",
+    "Nusrat Fateh ali kahn",
+];
+function showMAgisions(magicio) {
+    magicio.forEach(function (magi) { return console.log(magi); });
+}
+showMAgisions(magicion);
---
+++ day-14/q41/main.ts
@@ -0,0 +1,11 @@
+let magicion: string[] = [
+  "Chahat fateh ali khan",
+  "RAhat fateh ali khan",
+  "Nusrat Fateh ali kahn",
+];
+
+function showMAgisions(magicio: string[]) {
+  magicio.forEach((magi) => console.log(magi));
+}
+
+showMAgisions(magicion);
