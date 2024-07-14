490859b6c3c9ddae09b7470117dd29601ba59326
Bilalraza617 <anwarbilal617@gmail.com>
2024-07-14 07:26:35 +0500

    complete

--- day-13/q39/main.js
+++ day-13/q39/main.js
@@ -1,7 +1,6 @@
-function city_country(city: string, country: string): string {
-  return `${city}, ${country}`;
+function city_country(city, country) {
+    return "".concat(city, ", ").concat(country);
 }
-
 console.log(city_country("'Lahore'", "'Pakistan'"));
 console.log(city_country("'Tokyo'", "'Japan'"));
 console.log(city_country("'Paris'", "'France'"));
